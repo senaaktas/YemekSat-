22f36b39d9f323b012b3ce1e60ec0239b1e341f3
Sena Aktaş <senaaktas93@gmail.com>
2018-03-13 09:18:59 +0300

    Tasarım değiştirildi yeni layout tasarlandı

---
+++ YemekSatış/Controllers/uyeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YemekSatış.Controllers
+{
+    public class uyeController : Controller
+    {
+        // GET: uye
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
