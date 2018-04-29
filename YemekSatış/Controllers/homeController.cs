26e78f10935196f330b691b92b36c5010dde6a26
Sena Aktaş <senaaktas93@gmail.com>
2018-04-29 18:38:32 +0300

    Menü kısımları eklendidatabase bağlantıları sağlandı menü detayları listelendi

--- YemekSatış/Controllers/homeController.cs
+++ YemekSatış/Controllers/homeController.cs
@@ -18,11 +18,33 @@ namespace YemekSatış.Controllers
         }
         public JsonResult MutfakListesiYukle()
         {
-            List<CategoriesListDTO> mutfak = db.food_catagory.Select(s=> new CategoriesListDTO {
-                food_catagory_id=s.food_catagory_id,
-                food_catagory_name=s.food_catagory_name,
+            List<CategoriesListDTO> mutfak = db.food_catagory.Select(s => new CategoriesListDTO
+            {
+                food_catagory_id = s.food_catagory_id,
+                food_catagory_name = s.food_catagory_name,
             }).ToList();
             return Json(mutfak, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Mutfaklar(int id)
+        {
+            List<SubCategoriesListDTO> list = db.foods.Where(x => x.food_catagory_id == id).Select(s => new SubCategoriesListDTO
+            {
+                CategoryName = s.food_catagory.food_catagory_name,
+                food_name = s.food_name,
+                food_catagory_id=s.food_catagory_id,
+                food_id = s.food_id,
+            }).OrderBy(z => z.food_name).ToList();
+
+            return View(list);
+        }
+        public ActionResult MutfakDetay(int anakategori, int altkategori)
+        {
+            List<DetailsListDTO> list = db.Details.Where(x => x.KategoriID == anakategori && x.AltKategoriID == altkategori).Select(s => new DetailsListDTO
+            {
+                Id = s.Id,
+                Details1 = s.Details1,
+            }).OrderBy(k => k.Details1).ToList();
+            return View(list);
         }
     }
 }
